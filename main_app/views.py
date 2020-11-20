60de977338d1de67838c6528644fa992ead05c96
Jermaine <jermzblake@gmail.com>
2020-11-20 16:35:13 -0500

    add total time

--- main_app/views.py
+++ main_app/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 from django.views.generic.edit import DeleteView
+from django.db.models import Sum
 
 def tasks_index(request):
     task_list = Task.objects.all()
     #instantiate TaskForm to be rendered in the template
     task_form = TaskForm()
+    total_time = Task.objects.aggregate(Sum('time'))
     return render(request, 'index.html', {
         'task_list': task_list,
-        'task_form':task_form
+        'task_form':task_form,
+        'total_time':total_time
         })
 
 def add_task(request):
