f183e5a2ed260ddd615aeace4883ddceb5ccba4b
Jermaine <jermzblake@gmail.com>
2020-11-20 14:26:55 -0500

    add TaskForm

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 
 def tasks_index(request):
     task_list = Task.objects.all()
-    return render(request, 'index.html', {'task_list': task_list})
+    #instantiate TaskForm to be rendered in the template
+    task_form = TaskForm()
+    return render(request, 'index.html', {
+        'task_list': task_list,
+        'task_form':task_form
+        })
